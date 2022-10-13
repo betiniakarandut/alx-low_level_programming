00d2e74cb567ca86d74849e553178b09ce588b30
betiniakarandut <betiniakarandut@gmail.com>
2022-10-13 15:00:25 -0700

    Using printf

---
+++ 0x00-hello_world/5-printf.c
@@ -0,0 +1,11 @@
+#!/bin/bash
+#include <stdio.h>
+
+/**
+ * print_grammar - Prints "with proper grammar, but the outcome is a piece of art"
+ */
+void print_grammar(void)
+{
+	printf("with proper grammar, but the outcome is a piece of art");
+	return (0);
+}
