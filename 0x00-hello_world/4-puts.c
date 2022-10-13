d89d83957d698cc0d59ddec659bb03b76cf3c5b6
betiniakarandut <betiniakarandut@gmail.com>
2022-10-13 14:25:38 -0700

    Programming is fun

--- 0x00-hello_world/4-puts.c
+++ 0x00-hello_world/4-puts.c
@@ -1,10 +1,8 @@
 #include <stdio.h>
 
  /**
-  * main - Outputs a string using put function
-  * Return - A string "Programming is like building a multilingual puzzle
+  * main - Prints exactly "Programming is like building a multilingual puzzle
   */
-
 int main(void)
 {
 	puts("\"Programming is like building a multilingual puzzle");
