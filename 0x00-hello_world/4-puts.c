55562edb7462b4925e244d36ff16a304396bdb32
betiniakarandut <betiniakarandut@gmail.com>
2022-10-13 13:54:01 -0700

    Uploading files

---
+++ 0x00-hello_world/4-puts.c
@@ -0,0 +1,12 @@
+#include <stdio.h>
+
+ /**
+  * main - Outputs a string using put function
+  * Return - A string "Programming is like building a multilingual puzzle
+  */
+
+int main(void)
+{
+	put("\"Programming is like building a multilingual puzzle\n");
+	return(0);
+}
