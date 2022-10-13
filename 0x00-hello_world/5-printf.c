850c07bb80aa856b6f36826a344c3ec80784b98d
betiniakarandut <betiniakarandut@gmail.com>
2022-10-13 15:09:02 -0700

    Using printf

--- 0x00-hello_world/5-printf.c
+++ 0x00-hello_world/5-printf.c
@@ -4,7 +4,7 @@
  * print_grammar - Prints "with proper grammar, but the outcome is a piece of art"
  * return: 0 Always (Success)
  */
-int print_grammar(void)
+int main(void)
 {
 	printf("with proper grammar, but the outcome is a piece of art");
 	return (0);
