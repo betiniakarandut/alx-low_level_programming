fccd5849cee8d3a769de490d9ccc2715aab592b6
betiniakarandut <betiniakarandut@gmail.com>
2022-10-14 13:56:49 -0700

    Print capital and small letters

---
+++ 0x01-variables_if_else_while/3-print_alphabets.c
@@ -0,0 +1,22 @@
+#include <stdio.h>
+/**
+ * main - Function that print capital and small
+ * letters.
+ * Description: The for loop iterate over the
+ * letters of alphabet.
+ * Return: Always 0 (Success).
+ */
+int main (void){
+	char small;
+	char big;
+	for(small = 'a'; small <= 'z'; small++)
+	{
+		putchar(small);
+		for (big = 'A'; big = 'Z'; big++)
+		{
+			putchar(big);
+		}
+	}
+	putchar('\n');
+	return (0)
+}
