d031516ac1b4486027120e9cb2a3b2f36d733de5
betiniakarandut <betiniakarandut@gmail.com>
2022-10-14 14:59:24 -0700

    Numbers in base ten

---
+++ 0x01-variables_if_else_while/5-print_numbers.c
@@ -0,0 +1,18 @@
+#include <stdio.h>
+
+/**
+ * main - Function to print numbers in base 10.
+ * Description: for loop for iterating
+ * over the digit.
+ * Return: Always 0 (Success).
+ */
+int main(void)
+{
+	int num;
+
+	for (num = 0; num <= 10; num++)
+	{
+		printf("%d\n", num);
+	}
+	return (0);
+}
