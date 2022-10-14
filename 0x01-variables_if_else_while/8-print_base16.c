f9ee077d83bacca18a90a7d0d5f4eaf8bbce86e1
betiniakarandut <betiniakarandut@gmail.com>
2022-10-14 16:31:05 -0700

    Print hex in lowercase letters

---
+++ 0x01-variables_if_else_while/8-print_base16.c
@@ -0,0 +1,17 @@
+#include <stdio.h>
+/**
+ * main - Function to print all numbers in base 16
+ * Return: Always 0 (Success)
+ */
+int main(void)
+{
+	int nu;
+	char lt;
+
+	for (nu = 0; nu <=10; nu++)
+		putchar(nu);
+	for (lt = 'a'; lt <= 'f'; lt++)
+		putchar(lt);
+	putchar('\n');
+	return (0);
+}
