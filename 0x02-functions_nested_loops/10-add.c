b85510aca4976604115769bacb05a54424a48ae4
betiniakarandut <betiniakarandut@gmail.com>
2022-10-18 16:51:54 -0700

    summation of integer values

---
+++ 0x02-functions_nested_loops/10-add.c
@@ -0,0 +1,16 @@
+#include "main.h"
+/**
+ * add - function that adds two integers and returns the resul
+ * @a: Character to check
+ * @b: character to check
+ * Return: a + b (Success)
+ */
+int add(int a, int b)
+{
+	int c;
+
+	c = a + b;
+
+	return (c);
+}
+
