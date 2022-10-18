6d613de3016867764a652aa81e40f4a1cd363ddf
betiniakarandut <betiniakarandut@gmail.com>
2022-10-18 12:08:53 -0700

    Print 10x

--- 0x02-functions_nested_loops/2-print_alphabet_x10.c
+++ 0x02-functions_nested_loops/2-print_alphabet_x10.c
@@ -6,13 +6,12 @@
  */
 void print_alphabet_x10(void)
 {
-	char ch;
-	ch = 'a';
 	int num;
+	char ch;
 
 	for (num = 0; num <= 10; num++)
 	{
-		for (ch <= 'z'; ch++)
+		for (ch = 'a'; ch <= 'z'; ch++)
 		{
 			_putchar(ch);
 		}
