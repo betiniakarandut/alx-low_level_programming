e736a86f43538f999d4940fe94bae84247747a25
betiniakarandut <betiniakarandut@gmail.com>
2022-10-18 14:17:51 -0700

    Absolute values of integers

---
+++ 0x02-functions_nested_loops/6-abs.c
@@ -0,0 +1,17 @@
+#include "main.h"
+
+/**
+ * _abs - Function that computes the absolete
+ * value of an integer
+ * @a: character to check for
+ * Return: bon
+ */
+int _abs(int a)
+{
+	if (a >= 0)
+	{
+		return (a);
+	}
+	return (-a);
+}
+
