47d23c84aca5ecac43da6b7fb64175e36233a9bd
betiniakarandut <betiniakarandut@gmail.com>
2022-10-18 04:07:45 -0700

    Lowercase alphabet

---
+++ 0x02-functions_nested_loops/1-alphabet.c
@@ -0,0 +1,14 @@
+#include "print_alphabet.h"
+/**
+ * main - check the code
+ *
+ * Return: Always 0.
+ */
+int main(void)
+{
+   void print_alphabet(void);
+   _putchar(a..z);
+   _putchar('\n');
+
+    return (0);
+}
