42a28a94bc75d14e1ed94655ed0f6753da4f3cd3
betiniakarandut <betiniakarandut@gmail.com>
2022-10-18 13:04:46 -0700

    To lowercase

--- 0x02-functions_nested_loops/3-islower.c
+++ 0x02-functions_nested_loops/3-islower.c
@@ -1,10 +1,11 @@
 #include "main.h"
 /**
  * _islower - Function that changes to lowercase
+ * @c: character to check
  * Return: Return 1 if it is true and
- * 0 if not true 
+ * 0 if not true
  */
 int _islower(int c)
-{	
+{
 	return (c >= 'a' && c <= 'z');
 }
