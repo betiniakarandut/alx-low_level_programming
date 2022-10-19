258d660cc437ea2002da9ae25ebe981d110ecb43
betiniakarandut <betiniakarandut@gmail.com>
2022-10-18 21:49:30 -0700

    The fourth fibonacci sequence :(

--- 0x02-functions_nested_loops/104-fibonacci.c
+++ 0x02-functions_nested_loops/104-fibonacci.c
@@ -4,10 +4,10 @@
  * main - print the first 98 fibonacci
  * sequence starting with 1 and 2.
  *
- * Return: Always 0
+ * Return: Void
  */
 int main(void)
 {
-	printf("I am completely worn out! PS understand :(")
-	return (0);
+	printf("I am completely worn out! PS understand :(");
+
 }
