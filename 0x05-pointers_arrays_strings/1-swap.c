beda597adf5cbda10e41ad5dd08dcf45f1e886c4
betiniakarandut <betiniakarandut@gmail.com>
2022-10-24 10:08:57 -0700

    Swapping int values

---
+++ 0x05-pointers_arrays_strings/1-swap.c
@@ -0,0 +1,25 @@
+#include "main.h"
+
+/**
+ * swap_int - Function that swaps value of two integers.
+ *
+ * @*a: Character pointer to check
+ *
+ * @*b: Character pointer to check
+ *
+ * Return: Swap values of any int variable
+ */
+void swap_int(int *a, int *b)
+{
+	*a = 45;
+	*b = 90;
+
+	if (*a)
+	{
+		return((*b = *a));
+	}
+	else if (*b)
+	{
+		return((*a = *b));
+	}
+}
