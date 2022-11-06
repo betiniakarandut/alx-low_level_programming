7b3822ae285d7ad83fd6bb1fd52daec32fa64e98
betiniakarandut <betiniakarandut@gmail.com>
2022-11-06 15:14:52 -0800

    Creating a static library

---
+++ 0x09-static_libraries/lib_1-isdigit.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+
+int _isdigit(int c)
+{
+	printf("%d", c);
+}
---
+++ 0x09-static_libraries/lib_6-abs.c
@@ -0,0 +1,7 @@
+#include "main.h"
+#include <stdio.h>
+
+int _abs(int n)
+{
+	printf("%d", n);
+}
