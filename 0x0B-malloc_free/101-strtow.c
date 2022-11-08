b708aa1faf8abb2a812046df340a62686a8f0edf
betiniakarandut <betiniakarandut@gmail.com>
2022-11-08 13:26:24 -0800

    Splits a string into words

--- 0x0B-malloc_free/101-strtow.c
+++ 0x0B-malloc_free/101-strtow.c
@@ -1,23 +1,75 @@
-#include "main.h"
 #include <stdlib.h>
+#include "holberton.h"
 
 /**
- * free_grid - frees the memory allocate for the grid
- * created by alloc_grid()
- * 
- * @grid: grid to free
- * 
- * @height: height of the grid
+ * count_word - helper function to count the number of words in a string
+ * @s: string to evaluate
+ * Return: number of words
  */
-void free_grid(int **grid, int height)
+int count_word(char *s)
 {
-	int a;
+	int flag, c, w;
 
-	if (grid == NULL || height == 0)
-		return;
+	flag = 0;
+	w = 0;
 
-	for (a = 0; a < height; a++)
-		free(grid[a]);
+	for (c = 0; s[c] != '\0'; c++)
+	{
+		if (s[c] == ' ')
+			flag = 0;
+		else if (flag == 0)
+		{
+			flag = 1;
+			w++;
+		}
+	}
 
-	free(grid);
+	return (w);
+}
+/**
+ * **strtow - splits a string into words
+ * @str: string to split
+ * Return: pointer to an array of strings (Success)
+ * or NULL (Error)
+ */
+char **strtow(char *str)
+{
+	char **matrix, *tmp;
+	int i, k = 0, len = 0, words, c = 0, start, end;
+
+	while (*(str + len))
+		len++;
+	words = count_word(str);
+	if (words == 0)
+		return (NULL);
+
+	matrix = (char **) malloc(sizeof(char *) * (words + 1));
+	if (matrix == NULL)
+		return (NULL);
+
+	for (i = 0; i <= len; i++)
+	{
+		if (str[i] == ' ' || str[i] == '\0')
+		{
+			if (c)
+			{
+				end = i;
+				tmp = (char *) malloc(sizeof(char) * (c + 1));
+				if (tmp == NULL)
+					return (NULL);
+				while (start < end)
+					*tmp++ = str[start++];
+				*tmp = '\0';
+				matrix[k] = tmp - c;
+				k++;
+				c = 0;
+			}
+		}
+		else if (c++ == 0)
+			start = i;
+	}
+
+	matrix[k] = NULL;
+
+	return (matrix);
 }
