f70e9944feed0208d630611ed1b5d88e8ee20986
betiniakarandut <betiniakarandut@gmail.com>
2022-11-08 12:18:18 -0800

    Free 2-D memory allocation

--- 0x0B-malloc_free/4-free_grid.c
+++ 0x0B-malloc_free/4-free_grid.c
@@ -4,9 +4,9 @@
 /**
  * free_grid - frees the memory allocate for the grid
  * created by alloc_grid()
- * 
+ *
  * @grid: grid to free
- * 
+ *
  * @height: height of the grid
  */
 void free_grid(int **grid, int height)
