6cd5b063c0d332d4e4b7107cb039d9c17229766e
betiniakarandut <betiniakarandut@gmail.com>
2022-11-08 11:44:02 -0800

    Duplicate copy

--- 0x0B-malloc_free/1-strdup.c
+++ 0x0B-malloc_free/1-strdup.c
@@ -9,7 +9,6 @@
  *
  * Return: pointer to the copied string (Success), NULL (Error)
  */
-
 char *_strdup(char *str)
 {
 	char *dup;
@@ -19,18 +18,26 @@ char *_strdup(char *str)
 	len = 0;
 
 	if (str == NULL)
+	{
 		return (NULL);
+	}
 
 	while (str[len])
+	{
 		len++;
+	}
 
 	dup = malloc(sizeof(char) * (len + 1));
 
 	if (dup == NULL)
+	{
 		return (NULL);
+	}
 
 	while ((dup[i] = str[i]) != '\0')
+	{
 		i++;
+	}
 
 	return (dup);
 }
