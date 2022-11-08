e62945660fe5ab58d7e1de2780a0959a42a053f0
betiniakarandut <betiniakarandut@gmail.com>
2022-11-08 05:26:29 -0800

    Creating array of given size

--- 0x0B-malloc_free/0-create_array.c
+++ 0x0B-malloc_free/0-create_array.c
@@ -20,7 +20,7 @@ char *create_array(unsigned int size, char c)
 	{
 		return (NULL);
 	}
-	arr = (char *) malloc(sizeof(char)* size);
+	arr = (char *) malloc(sizeof(char) * size);
 
 	if (arr == 0)
 	{
