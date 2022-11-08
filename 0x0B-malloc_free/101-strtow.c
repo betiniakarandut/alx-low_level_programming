b29b1b30f06d0f1b76365fe14216b5ccfcccf3aa
betiniakarandut <betiniakarandut@gmail.com>
2022-11-08 13:14:31 -0800

    Splits a string into words

---
+++ 0x0B-malloc_free/101-strtow.c
@@ -0,0 +1,23 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * free_grid - frees the memory allocate for the grid
+ * created by alloc_grid()
+ * 
+ * @grid: grid to free
+ * 
+ * @height: height of the grid
+ */
+void free_grid(int **grid, int height)
+{
+	int a;
+
+	if (grid == NULL || height == 0)
+		return;
+
+	for (a = 0; a < height; a++)
+		free(grid[a]);
+
+	free(grid);
+}
