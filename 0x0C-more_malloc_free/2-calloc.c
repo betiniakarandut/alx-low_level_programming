b4c28e7773340bf81fe238e5baecaeedbacca71e
betiniakarandut <betiniakarandut@gmail.com>
2022-11-10 18:07:32 -0800

    Allocate some space for the array

---
+++ 0x0C-more_malloc_free/2-calloc.c
@@ -0,0 +1,44 @@
+#include "main.h"
+#include <stdlib.h>
+
+/**
+ * *_memset - fills memory with a constant byte
+ * @s: memory area to be filled
+ * @b: char to copy
+ * @n: number of times to copy b
+ * Return: pointer to the memory area s
+ */
+char *_rewindmem(char *s, char b, unsigned int n)
+{
+	unsigned int i;
+
+	for (i = 0; i < n; i++)
+	{
+		s[i] = b;
+	}
+
+	return (s);
+}
+
+/**
+ * *_calloc - allocates memory for an array
+ * @nmemb: number of elements in the array
+ * @size: size of each element
+ * Return: pointer to allocated memory
+ */
+void *_calloc(unsigned int nmemb, unsigned int size)
+{
+	char *betini;
+
+	if (nmemb == 0 || size == 0)
+		return (NULL);
+
+	betini = malloc(size * nmemb);
+
+	if (betini == NULL)
+		return (NULL);
+
+	_rewindmem(betini, 0, nmemb * size);
+
+	return (betini);
+}
