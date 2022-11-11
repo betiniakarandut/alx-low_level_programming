5fac4f3e66168fde59c8ea34f2c1ee53c31b55b8
betiniakarandut <betiniakarandut@gmail.com>
2022-11-10 20:18:11 -0800

    Join the two strings

--- 0x0C-more_malloc_free/1-string_nconcat.c
+++ 0x0C-more_malloc_free/1-string_nconcat.c
@@ -2,13 +2,12 @@
 #include <stdlib.h>
 
 /**
- * *string_nconcat - concatenates n bytes of a string
- * to another string
+ * *string_nconcat - concatenates n bytes of a
+ * string to another string
  * @s1: string to append to
- * 
  * @s2: string to concatenate from
- * @n: number of bytes from s2 to concatenate to s1
- *
+ * @n: number of bytes from s2 to
+ * concatenate to s1
  * Return: pointer to the resulting string
  */
 char *string_nconcat(char *s1, char *s2, unsigned int n)
