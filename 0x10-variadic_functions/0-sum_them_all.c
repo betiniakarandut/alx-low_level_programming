f3aaac6679eae8387d345ede06a632ececb24cea
betiniakarandut <betiniakarandut@gmail.com>
2022-11-17 15:15:07 -0800

    Take the sum of all the arguments

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -1,6 +1,7 @@
 #include "variadic_functions.h"
 /**
- * sum_them_all - returns the sum of all its parameters
+ * sum_them_all - returns the sum of
+ * all its parameters
  * @n: int parameter to check
  * Return: Always 0 (Success)
  */
