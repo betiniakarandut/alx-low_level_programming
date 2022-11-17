83450aee27621ba46f89d7fc8f32b74108d10882
betiniakarandut <betiniakarandut@gmail.com>
2022-11-17 15:40:30 -0800

    Take the summ of all the arguments passed

--- 0x10-variadic_functions/0-sum_them_all.c
+++ 0x10-variadic_functions/0-sum_them_all.c
@@ -3,7 +3,7 @@
  * sum_them_all - returns the sum of
  * all its parameters
  * @n: int parameter to check
- * Return: Always 0 (Success)
+ * Return: calculated sum
  */
 int sum_them_all(const unsigned int n, ...)
 {
