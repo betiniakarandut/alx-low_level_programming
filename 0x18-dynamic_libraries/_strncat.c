278148c440eeb39f9f2acba6372c1cef10e52469
betiniakarandut <betiniakarandut@gmail.com>
2023-01-16 18:24:52 +0000

    Changing codes for duplicated functions

--- 0x18-dynamic_libraries/_strncat.c
+++ 0x18-dynamic_libraries/_strncat.c
@@ -1,7 +1,7 @@
 #include "main.h"
 #include <stdio.h>
 
-char *_strcat(char *dest, char *src)
+char *_strcat(char *dest, char *src, int n)
 {
-	printf("%c%c", *dest, *src);
+	printf("%c%c%d", *dest, *src, n);
 }
