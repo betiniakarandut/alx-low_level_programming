f40752b9a4fd6c588c266cb0dea0218914425c12
betiniakarandut <betiniakarandut@gmail.com>
2023-05-11 23:21:53 +0000

    Added: condition check

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -12,7 +12,7 @@ int linear_search(int *array, size_t size, int value)
 {
 	size_t i = 0;
 
-        if(array == NULL)
+        if(!array || array == NULL)
                 return (-1);
 
         while(i < size)
