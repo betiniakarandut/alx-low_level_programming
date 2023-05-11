5c93c61b723975809602026b318a2aa77e052874
betiniakarandut <betiniakarandut@gmail.com>
2023-05-11 20:56:16 +0000

    Compiled: gcc -Wall -Werror...

--- 0x1E-search_algorithms/0-linear.c
+++ 0x1E-search_algorithms/0-linear.c
@@ -1,28 +1,35 @@
-#include "search_algos.h"
 #include <stdio.h>
 #include <stdlib.h>
-#include <unistd.h>
-
 /**
- * linear_search - searches for a value in an array of
- * integers using the Linear search algorithm
- * @array: array to search the value in
- * @size: size of the array
- * @value: value to look for
- *
- * Return: the index of the found value,
- * or -1 if not found
- */
+ * linear_search - searches for a value in an array
+ * @array: pointer to first element of array to search in
+ * @size: size of array
+ * @value: int to check
+ * Return: -1(for a NULL value)
+ * */
 int linear_search(int *array, size_t size, int value)
 {
-    if (array == NULL) {
-        return (-1);
-    }
-    for (size_t i = 0; i < size; i++) {
-        printf("Value checked array[%lu] = [%d]\n", i, array[i]);
-        if (array[i] == value) {
-            return (i);
+        long unsigned i;
+
+        if(array == NULL)
+                return (-1);
+
+        for(i = 0; i < size; i++)
+        {
+                printf("Value checked index array[%lu] = [%d]\n", i, array[i]);
+                if(array[i] == value)
+                        return(i);
         }
-    }
-    return (-1);
+        return (-1);
+}
+
+int main(void)
+{
+        int array[] = {
+                10, 1, 42, 3, 4, 42, 6, 7, -1, 9
+        };
+
+        size_t size = sizeof(array) / sizeof(array[0]); //4*10/4*1 = 10
+        printf("Found %d at index: %d\n\n", 3, linear_search(array, size, 3));
+        return(1);
 }
